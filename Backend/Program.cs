27ea18d36139268b5ef3a824a1d0e2ebaf24ea49
Henning Mæland <henning.maeland@volue.com>
2021-12-02 19:38:10 +0100

    Add root endpoint to test if app is up and running

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,7 +1,9 @@
 using Backend;
 using Backend.Data;
 using Backend.Models;
+using System.Diagnostics;
 
+var startTime = DateTime.UtcNow;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -28,3 +30,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+app.MapGet("/", (IWebHostEnvironment env) => {
+    var aspNetCoreAssembly = typeof(Microsoft.AspNetCore.Builder.WebApplication).Assembly;
+    var fileVersionInfo = FileVersionInfo.GetVersionInfo(aspNetCoreAssembly.Location);
+    var baseDirectory = System.AppContext.BaseDirectory;
+    return $".NET 6 Minimal API is up and running!" + Environment.NewLine +
+            $"AspNetCore Version: {fileVersionInfo.FileVersion} (Location: {aspNetCoreAssembly.Location})" + Environment.NewLine +
+            $"Base directory: {baseDirectory}" + Environment.NewLine +
+            $"Startup time (UTC): {startTime:o}" + Environment.NewLine +
+            $"Environment name: {env.EnvironmentName}" + Environment.NewLine;
+}).WithName("RootEndpoint");
